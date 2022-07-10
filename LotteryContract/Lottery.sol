e60e205a1709b3032d502bee7e43430de243164d
ngocphuphamm <ngocphupham682001@gmail.com>
2022-07-10 14:50:54 +0700

    lottery

---
+++ LotteryContract/Lottery.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract Lottery{
+    address public manager ;
+      address payable[] public players;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+    function enter() payable public{
+        // .01 = 0.01 ether;
+        require(
+            msg.value > .01 ether,
+            "A minimum payment of .01 ether must be sent to enter the lottery"
+        );
+        players.push(payable(msg.sender));
+    }
+
+    function getArrayPlayersLength() public view returns(uint){
+        return players.length;
+    }
+
+    function random() private  view returns (uint){
+        return  uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players)));
+    }
+
+    function pickWinner() public{
+        uint index = random() % players.length;
+        address contractAddress = address(this);
+        players[index].transfer(contractAddress.balance);
+        players = new address payable[](0);
+    }
+}
